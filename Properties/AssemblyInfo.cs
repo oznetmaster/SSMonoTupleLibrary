0625754d4236668fb353f1a657cf9338c133ee21
oznetmaster <ncolvin@nivloc.com>
2014-12-23 08:36:25 +0800

    Update to latest S# Release

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle ("SSMonoTupleLibrary")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("SSMonoTupleLibrary")]
+[assembly: AssemblyCopyright ("Copyright ©  2014")]
+[assembly: AssemblyVersion ("1.0.0.*")]
+
